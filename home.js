7f286063131bbd5f095d045331ca1a673981c081
ButtonCard <dcmetro326@gmail.com>
2025-03-19 17:55:34 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,18 +1,5 @@
 //PACK OPENING FUNCTIONS
 
-//Store Info About Pack Opened
-let packSizes = [4,3,4];
-let packCost = [1,1,2];
-let num = 0;
-let pack = [];
-
-//Cards Available Sorted by Rarity:
-let C = ["01-1", "01-2"];
-let U = ["01-3", "01-4"];
-let R = ["01-5", "01-6"];
-let E = ["01-7"];
-let L = ["01-8"];
-
 //Loads in Pack Token Count
 async function loadTokens() {
   const currentUser = JSON.parse(localStorage.getItem('currentUser'));
