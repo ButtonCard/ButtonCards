5f8dff38ac39fd22940e801d6d976a26fad24946
ButtonCard <dcmetro326@gmail.com>
2025-03-19 18:09:37 -0400

    Update cardStoreInfo.js

--- cardStoreInfo.js
+++ cardStoreInfo.js
@@ -2,6 +2,12 @@
 let packSizes = [4,3,4];
 let packCost = [1,1,2];
 
+//Pack 1 Image
+document.getElementByID('Pack1')='Pack1.png';
+//Pack 2 Image
+document.getElementByID('Pack2')='Pack2.png';
+//Pack 3 Image
+document.getElementByID('Pack3')='Pack3.png';
 
 //All Cards Available Sorted by Rarity:
 let C = ["01-1", "01-2"];
