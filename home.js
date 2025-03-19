3a06d3335826aaf006d1cbd144697b3da3c65017
ButtonCard <dcmetro326@gmail.com>
2025-03-19 02:37:01 -0400

    Update home.js

--- home.js
+++ home.js
@@ -22,7 +22,7 @@ window.onload = async function() {
       .get();
   const doc = userQuery.docs[0];
   let tokens = doc.data().tokens;
-  document.getElementById('tokenCount').innerHTML = 'Pack Tokens: ' + tokens;
+  document.getElementById('tokenCount').textContent = `Pack Tokens: ` + tokens;
 }
 
 //Checks for Token, Opens Pack and Changes Page to Start Pack Opening Sequence
