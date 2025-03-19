ae1e27de1c28dc2cfc78d508e02b7394138e8b29
ButtonCard <dcmetro326@gmail.com>
2025-03-19 18:06:29 -0400

    Create cardStoreInfo.js

---
+++ cardStoreInfo.js
@@ -0,0 +1,17 @@
+//Store Info About Pack Opened
+let packSizes = [4,3,4];
+let packCost = [1,1,2];
+
+
+//All Cards Available Sorted by Rarity:
+let C = ["01-1", "01-2"];
+let U = ["01-3", "01-4"];
+let R = ["01-5", "01-6"];
+let E = ["01-7"];
+let L = ["01-8"];
+
+
+//All Sets Current Available
+let List01 = ["01-1", "01-2", "01-3", "01-4", "01-5", "01-6", "01-7", "01-8", "01-A"];
+let List02 = ["01-4", "01-5", "01-6"];
+let allSets = [null, List01, List02];
