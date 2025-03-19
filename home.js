c661b3d661d14d952f7c65525c779b6923730004
ButtonCard <dcmetro326@gmail.com>
2025-03-19 03:08:44 -0400

    Update home.js

--- home.js
+++ home.js
@@ -22,7 +22,7 @@ async function loadTokens() {
       .get();
   const doc = userQuery.docs[0];
   let tokens = doc.data().tokens;
-  document.getElementByClass('.tokenCount').textContent = document.getElementByClass('.tokens').textContent;
+  document.getElementByClassName('.tokenCount').textContent = document.getElementByClassName('.tokens').textContent;
 }
 loadTokens();
 
