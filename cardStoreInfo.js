7f4ed0ee898c972070245e6263623ffc2691e29d
ButtonCard <dcmetro326@gmail.com>
2025-03-21 17:39:45 -0400

    Update cardStoreInfo.js

--- cardStoreInfo.js
+++ cardStoreInfo.js
@@ -5,18 +5,18 @@ let packCost = [1,1,2];
 
 //Pack 1 Info
 let Pack1_Name='Standard Pack';
-let Pack1_Description='All Standard Cards Available<br>20% Chance for St. Patricks Day Variant<br>5% Chance for Spring Variant<br>4 Cards<br>Cost: 1 Pack Token';
-let Pack1_Image='Pack1.png';
+let Pack1_Description='All Standard Cards Available<br>Chance for First Edition Variant<br>Chance for BUTTON Meme Special Cards<br>4 Cards<br>Cost: 1 Pack Token';
+let Pack1_Image='icons/Pack_Icon.png';
 let Pack1_Time=1742936120000; //Far Future
 //Pack 2 Info
-let Pack2_Name='St. Patricks Day Exclusive Variant Pack';
-let Pack2_Description='50% Chance for St. Patricks Day Variant!<br>For Sale: Until 3/17/2025<br>3 Cards<br>Cost: 1 Pack Token';
-let Pack2_Image='Pack2.png';
+let Pack2_Name='First Edition Variant Pack';
+let Pack2_Description='50% Chance for First  Variant!<br>For Sale: Until 3/23/2025<br>3 Cards<br>Cost: 1 Pack Tokens';
+let Pack2_Image='icons/First_Icon.png';
 let Pack2_Time=1742992400000; //3/20
 //Pack 3 Info
 let Pack3_Name='Pack Not Available';
 let Pack3_Description='TBA<br>For Sale: Until N/A<br>N/A Cards<br>Cost: N/A Pack Token';
-let Pack3_Image='Pack3.png';
+let Pack3_Image='icons/Null_Icon.png';
 let Pack3_Time=1741658400000; //3/19
 
 // ----- CARDS -----
