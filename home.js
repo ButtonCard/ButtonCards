cc1ac95b66ac257a122f7d1ae43d438f825653eb
ButtonCard <dcmetro326@gmail.com>
2025-04-01 00:04:56 -0400

    Update home.js

--- home.js
+++ home.js
@@ -194,6 +194,22 @@ async function openPack(pack_Num) {
       }
     }
   }
+
+  //SPECIAL (April Fools Only) - Standard
+  if (packNum==0){
+    for (let i = 0; i < packSize; i++) {
+      if (inPack < packSize && Math.floor(Math.random() * 3) + 1 == 1) {
+
+        let randCard = S4[Math.floor(Math.random() * (S4.length))];
+        
+        if (backUp <= 99) {
+          pack.push(randCard + ".png");
+          inPack++;
+          console.log(randCard);
+        }
+      }
+    }
+  }
     
   //LEGENDARY
   for (let i = 0; i < packSize; i++) {
