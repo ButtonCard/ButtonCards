034ba0daac008043edf1c877f65580fc6c2411fb
ButtonCard <dcmetro326@gmail.com>
2025-04-02 18:56:23 -0400

    Update collectUser.js

--- collectUser.js
+++ collectUser.js
@@ -1,6 +1,122 @@
 //USER'S COLLECTION FUNCTIONS
 
+async function loadCards() {
+  const userRef = db.collection('users');
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  let prevCard = "";
+  let userCards = [];
 
+  let colList = document.querySelector('.collectionList');
+  let userList = colList.id;
+  if(userList=="all"){
+    awardChecker();
+  }
+  if(userList!=="all" && userList!=="sets" && userList!=="mission"){
+    console.log(userList);
+    const userQuery = await userRef
+          .where('name', '==', userList)
+          .get();
+    const doc = userQuery.docs[0];
+    userCards = doc.data().cards;
+    
+    let sortType = document.querySelector(".sortSelect");
+    if(sortType.value=="num"){
+      userCards=userCards.sort();
+    } else if(sortType.value=="numRev"){
+      userCards=userCards.sort();
+      userCards=userCards.reverse();
+    } else if(sortType.value=="rec"){
+      userCards=userCards.reverse();
+    } else if(sortType.value=="rar"){
+      userCards=sortByLastDigit(userCards.sort());
+    } else if(sortType.value!=="old"){
+      userCards=userCards.sort();
+    }
+    
+  } else if(userList=="mission"){
+    const missRef = db.collection('mission');
+    const missQuery = await missRef.get();
+    userCards = missQuery.docs[0].data().cards;
+    console.log(userCards);
+  } else{
+    const userQuery = await userRef.get();
+    userCards = userQuery.docs[0].data().cards.concat(
+      userQuery.docs[1].data().cards, 
+      userQuery.docs[2].data().cards, 
+      userQuery.docs[3].data().cards,
+      userQuery.docs[4].data().cards, 
+      userQuery.docs[5].data().cards).sort();
+  }
+  
+  if(userList=="sets"){
+    colList.style.display = "none";
+    loadSets(userCards);
+    return;
+  }
+  console.log(userCards);
+  let storeCards = userCards.slice();
+  
+  // Clear existing cards
+  colList.innerHTML = '';
+  
+  // Dynamically generate card elements for all cards
+  for (let i = 0; i < userCards.length; i++) {
+    // Create new list item and image for each card
+    const listItem = document.createElement('li');
+    const image = document.createElement('img');
+    
+    // Set image properties
+    image.className = `c${i}`;
+    image.src = "img/" + userCards[i] + ".png";
+    image.style.width = "110px";
+    image.style.border = "none";
+    image.setAttribute('onClick', `enlarge('c${i}')`);
+    
+    // Check for duplicate cards
+    if (userCards[i] == prevCard) {
+      let prevCardResult = document.querySelector(`.c${i - 1}`);
+      if (prevCardResult) {
+        prevCardResult.style.border = "2px yellow solid";
+      }
+      image.style.border = "2px yellow solid";
+    } else {
+      let curStr = storeCards.splice(i, 1);
+      if (storeCards.includes(curStr[0])) {
+        console.log(curStr[0]);
+        image.style.border = "2px yellow solid";
+      }
+      storeCards.splice(i, 0, curStr[0]);
+    }
+    
+    // Add image to list item
+    listItem.appendChild(image);
+    
+    // Add list item to collection list
+    colList.appendChild(listItem);
+    
+    prevCard = userCards[i];
+  }
+  
+  // Update or create the card count display
+  let countDisplay = document.getElementById('cardCount');
+  if (!countDisplay) {
+    countDisplay = document.createElement('div');
+    countDisplay.id = 'cardCount';
+    countDisplay.style.textAlign = 'center';
+    countDisplay.style.color = 'darkblue';
+    countDisplay.style.marginTop = '10px';
+    countDisplay.style.fontWeight = 'bold';
+    
+    // Insert count after the collection list
+    colList.parentNode.insertBefore(countDisplay, colList.nextSibling);
+  }
+  
+  // Update the count text
+  countDisplay.textContent = `Total Cards: ${userCards.length}`;
+}
+loadCards();
+
+/*
 //Loads in Cards to List from User's Collection
 async function loadCards() {
   const userRef = db.collection('users');
@@ -80,6 +196,7 @@ async function loadCards() {
   }
 }
 loadCards();
+*/
 
 //Loads All Sets if That Collect Sets Page is Opened
 async function loadSets(allCards) {
