d6bd1d5ee236f51907bb87af6dac3c0bb0a2af70
ButtonCard <dcmetro326@gmail.com>
2025-04-08 18:49:16 -0400

    Update collectUser.js

--- collectUser.js
+++ collectUser.js
@@ -439,7 +439,6 @@ async function awardChecker() {
     for(let num = 0; num < 6; num++){
       curCards=allQuery.docs[num].data().cards;
       console.log(curCards + " " + num);
-      console.log(cards);
       // Check if current User's Cards contains all requiredCards
       let containsAll = requiredCards.every(card => curCards.includes(card));
       let cardWithoutAorP = requiredCards[0].slice(0, -2); // Remove -1, -2, etc.
