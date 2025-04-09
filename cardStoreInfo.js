0d19c072a011cb5dc5f1fbc12a071113b7c8fc73
ButtonCard <dcmetro326@gmail.com>
2025-04-08 20:10:06 -0400

    Update cardStoreInfo.js

--- cardStoreInfo.js
+++ cardStoreInfo.js
@@ -71,14 +71,14 @@ let ListV1 = ["V1-1", "V1-2", "V1-3", "V1-4", "V1-5", "V1-6", "V1-7", "V1-8", "V
 let ListV2 = ["V2-1", "V2-2", "V2-3", "V2-4", "V2-5", "V2-6", "V2-7", "V2-8", "V2-A"];
 let ListV3 = ["V3-1", "V3-2", "V3-3", "V3-4", "V3-5", "V3-6", "V3-7", "V3-8", "V3-A"];
 let ListV4 = ["V4-1", "V4-2", "V4-3", "V4-4", "V4-5", "V4-6", "V4-7", "V4-8", "V4-A"];
-let ListV9 = ["V9-1"];
+let ListV9 = ["V9-1", "V9-2", "V9-3"];
 
 let ListS1 = ["S1-1", "S1-2", "S1-3", "S1-4", "S1-5", "S1-6", "S1-7", "S1-8", "S1-A"];
 let ListS2 = ["S2-1", "S2-2", "S2-3", "S2-4", "S2-5", "S2-6", "S2-7", "S2-8", "S2-A"];
 let ListS3 = ["S3-1", "S3-2", "S3-3", "S3-4", "S3-5", "S3-6", "S3-7", "S3-8", "S3-A"];
 let ListS4 = ["S4-1", "S4-2", "S4-3", "S4-4", "S4-5", "S4-6", "S4-7", "S4-8", "S4-A"];
 let ListS5 = ["S5-1", "S5-2", "S5-3", "S5-4", "S5-5", "S5-6", "S5-7", "S5-8", "S5-A"];
-let ListS9 = ["S9-1", "S9-2"];
+let ListS9 = ["S9-1", "S9-2", "S9-3"];
 let allSets = [null, List01, List02, List03, List04, List05, List06, List07, List08, List09, List10, List11, List12, List13, List14, List15, List16, ListV1, ListV2, ListV3, ListV4, ListV9, ListS1, ListS2, ListS3, ListS4, ListS5, ListS9];
 let awardSets = [List01, List02, List03, List04, List05, List06, List07, List08, List09, List10, List11, List12, List13, List14, List15, ListV4, ListS2];
 let missionSets = [C, U, R, E, V4];
